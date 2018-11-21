359492f3c9f190d967641bf8420c6108cdd212c7
Justin Hope <jhope@q30design.com>
2018-11-21 14:11:55 -0500

    added updated source from IESO

--- ieso-tou.js
+++ ieso-tou.js
@@ -1,25 +1,23 @@
 (function(){
 	var $rates = [ /* set your rates here */
 			{ 'Off' : 0.065 },
-			{ 'Mid' : 0.095 },
+			{ 'Mid' : 0.094 },
 			{ 'On' : 0.132 }
 		],
-		/* set your holidays here [month,day] */
-		$holidays = [
-			[9,4], [10,9], [12,25], [12,26]
+		$holidays = [ /* set your holidays here [month,day] */
+			[3,30], [5,21], [7,2], [8,6], [9,3], [10,8], [12,25], [12,26]
 		],
 		$seasons = {
 			"summer" : [5,1],
 			"winter" : [11,1]
 		},
-		$peaksfr = ['période creuse','période médiane','période de pointe'],
+		$peaksfr = ['pÃƒÂ©riode creuse','pÃƒÂ©riode mÃƒÂ©diane','pÃƒÂ©riode de pointe'],
 		$peakColors = [
 			{ 'background-color' : '#48a942', 'text-color' : '#48a942' },
 			{ 'background-color' : '#F8971D', 'text-color' : '#F8971D' },
 			{ 'background-color' : '#F53945', 'text-color' : '#F53945' }
 		],
-		/* set your tou usage pattern */
-		$usagePatterns = {
+		$usagePatterns = { /* set your tou usage pattern */
 			"summer" : [
 				'Off','Off','Off','Off','Off','Off','Off',
 				'Mid','Mid','Mid','Mid',
@@ -40,10 +38,10 @@
 		$container = false,
 		/* functions */
 		$season = function(){
-			var $s = '';
-			$date = new Date(),
-			$day = $date.getDate(),
-			$month = ( $date.getMonth() + 1 );
+			var $s = ''; 
+				$date = new Date(),
+				$day = $date.getDate(),
+				$month = ($date.getMonth() + 1);
 			// start with summer
 			if( $month >= $seasons['summer'][0] ){
 				$s = "summer";
@@ -153,84 +151,46 @@
 				'hour' : $fixhour($hourindex)
 			};
 		},
-
 		$output = function(){
-			/* check for output type */
-			var $scriptname  = 'ieso-tou.js',
-			    $scripts     = document.getElementsByTagName('script');
-
-			/* loop through scripts for a match */
-			for( var $s=0; $s < $scripts.length; $s++ ){
-				if(String($scripts[$s].src).indexOf($scriptname) > 0){
-					// check for data object attr
-					var attr = $($scripts[$s]).attr('data-output');
-					// For some browsers, `attr` is undefined; for others, `attr` is false. Check for both.
-					if (typeof attr !== typeof undefined && attr !== false) {
-					  // Element has this attribute
-						$html_output = false;
-						$data_output = window[attr];
-					}
-				}
-			}
-
 			/* loops */
 			var $pattern;
-			for( var $w=0; $w < $usagePatterns[ $season() ].length; $w++ ){
+			for(var $w=0; $w<$usagePatterns[$season()].length; $w++){
 				$pattern = !$isWeekend() ? $usagePatterns[$season()][$w] : 'Off';
 				$weekdays[$w] = $getrate($pattern);
 			}
-			var $ctou = $returnToU();
-			if($html_output){
-				var $html = '<div class="ctu-container">' +
-				'<span class="ctu-title">Current Time-of-Use Price</span>' +
-				'<div class="ctu-rates">' +
-				'<div class="ctu-current">' +
-				'<div class="ctu-info" style="background-color:' + $ctou['peakclass']['background-color'] + '; border-color:' + $ctou['peakclass']['background-color'] + ';">' +
-				'<span class="ctu-peak">' + $getpeak($ctou['current']) + '-Peak</span>' +
-				'<span class="ctu-peak-info"><strong>' + (($ctou['current'] * 100)).toFixed(1) + ' &cent;/kWh</strong> for ' + $ctou['hour'] + ' ' + $ctou['ampm'] + '</span>' +
-				'</div>' +
-				'</div>' +
-				'<div class="ctu-next">' +
-				'<div class="ctu-info" style="border-color:' + $ctou['nextpeakclass']['background-color'] + ';">' +
-				'<span class="ctu-peak">' + $getpeak($ctou['nextpeak']) + '-Peak Starts</span>' +
-				'<span class="ctu-peak-time">' + $ctou['nh'] + ' ' + $ctou['nampm'] + '</span>' +
-				'</div>' +
-				'</div>' +
-				'</div>' +
-				'<span class="ctu-credit"><strong>Digital presentation by:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.ieso.ca" target="_blank">www.ieso.ca</a></span>' +
-				'</div>';
-				if(!$container){
-					document.write('<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"><link href="http://www.ieso.ca/-/media/Files/IESO/tou/ieso-tou-css.css" rel="stylesheet"><div id="ctu-container"></div>');
-				}
-				$container = document.getElementById('ctu-container');
-				$container.innerHTML = $html;
-			}else{
-				var $data = {
-					"peakclass"     : $ctou['peakclass']['background-color'],
-					"currentpeak"   : $getpeak($ctou['current']),
-					"currentprice"  : (($ctou['current'] * 100)).toFixed(1),
-					"currenttime"   : $ctou['hour'],
-					"currentampm"   : $ctou['ampm'],
-					"nextpeakclass" : $ctou['nextpeakclass']['background-color'],
-					"nextpeak"      : $getpeak($ctou['nextpeak']),
-					"nextpeaktime"  : $ctou['nh'],
-					"nextampm"      : $ctou['nampm']
-				}
-				$data_output.tou = $data;
-				$data_output.init();
+			var $ctou = $returnToU(),
+			$html = '<div class="ctu-container">' + 
+			'<span class="ctu-title">Current Time-of-Use Price</span>' + 
+			'<div class="ctu-rates">' + 
+			'<div class="ctu-current">' + 
+			'<div class="ctu-info" style="background-color:' + $ctou['peakclass']['background-color'] + '; border-color:' + $ctou['peakclass']['background-color'] + ';">' + 
+			'<span class="ctu-peak">' + $getpeak($ctou['current']) + '-Peak</span>' + 
+			'<span class="ctu-peak-info"><strong>' + (($ctou['current'] * 100)).toFixed(1) + ' Â¢/kWh</strong> for ' + $ctou['hour'] + ' ' + $ctou['ampm'] + '</span>' + 
+			'</div>' + 
+			'</div>' + 
+			'<div class="ctu-next">' + 
+			'<div class="ctu-info" style="border-color:' + $ctou['nextpeakclass']['background-color'] + ';">' + 
+			'<span class="ctu-peak">' + $getpeak($ctou['nextpeak']) + '-Peak Starts</span>' + 
+			'<span class="ctu-peak-time">' + $ctou['nh'] + ' ' + $ctou['nampm'] + '</span>' + 
+			'</div>' + 
+			'</div>' + 
+			'</div>' + 
+			'<span class="ctu-credit"><strong>Digital presentation by:</strong>&nbsp;&nbsp;&nbsp;&nbsp;<a href="http://www.ieso.ca" target="_blank">www.ieso.ca</a></span>' + 
+			'</div>';
+			if(!$container){
+				document.write('<link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet"><link href="http://www.ieso.ca/-/media/Files/IESO/tou/ieso-tou-css.css" rel="stylesheet"><div id="ctu-container"></div>');
 			}
+			$container = document.getElementById('ctu-container');
+			$container.innerHTML = $html;
 		},
 		$resize = function(){
-			if($html_output){
-				$container = document.getElementById('ctu-container');
-				if($container.offsetWidth >= 768){ // move to two column
-					$container.classList.add('ctu-two-column');
-				}else{
-					$container.classList.remove('ctu-two-column');
-				}
+			$container = document.getElementById('ctu-container');
+			if($container.offsetWidth >= 768){ /* move to two column */
+				$container.classList.add('ctu-two-column');
+			}else{
+				$container.classList.remove('ctu-two-column');
 			}
 		};
-		var $html_output = true;
 		/* start */
 		$output();
 		$resize();
